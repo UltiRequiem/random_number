0974ed29e2f5d9246f01aa79c5e7398fe8dc9347
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-04 12:24:00 -0500

    :sparkles: Add some endpoints

--- src/api/mod.ts
+++ src/api/mod.ts
@@ -1,5 +1,46 @@
-import { serve } from "./api_deps.ts";
+import { serve, Status, STATUS_TEXT } from "./api_deps.ts";
 
-import { randomNumber } from "../core/mod.ts";
+import { randomNumber, randomNumberList } from "../core/mod.ts";
 
-serve(() => new Response(`${randomNumber()}`));
+function handler(request: Request): Response {
+  const { pathname } = new URL(request.url);
+
+  const data: {
+    body?: string;
+    headers: Record<string, string>;
+    status: number;
+  } = {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Request-Method": "*",
+      "Access-Control-Allow-Methods": "OPTIONS, GET",
+      "Access-Control-Allow-Headers": "*",
+    },
+    status: Status.OK,
+  };
+
+  switch (pathname) {
+    case "/":
+      data.body = `${randomNumber()}`;
+      break;
+    case "/decimal":
+      data.body = `${randomNumber({ integer: false })}`;
+      break;
+    case "/list":
+      data.body = `[${randomNumberList(10)}]`;
+      break;
+    case "/decimal/list":
+      data.body = `[${randomNumberList(10, { integer: false })}]`;
+      break;
+    default:
+      data.status = Status.NotFound;
+  }
+
+  return new Response(data.body ?? STATUS_TEXT.get(data.status), {
+    headers: data.headers,
+    status: data.status,
+    statusText: STATUS_TEXT.get(data.status),
+  });
+}
+
+serve(handler);
