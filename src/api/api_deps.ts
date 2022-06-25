f011e9fe37d85e3f1bb9604e37098424dd4a66b1
GitHub <noreply@github.com>
2022-06-25 00:10:41 +0000

    :arrow_up: Update Dependencies

--- src/api/api_deps.ts
+++ src/api/api_deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.144.0/http/server.ts";
-export * from "https://deno.land/std@0.144.0/http/http_status.ts";
+export * from "https://deno.land/std@0.145.0/http/server.ts";
+export * from "https://deno.land/std@0.145.0/http/http_status.ts";
