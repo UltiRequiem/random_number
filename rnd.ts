32358e4d66f1994200c2717262cc6eca93a022dc
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-07-21 14:47:26 -0500

    :sparkles: Add CLI Tool

--- rnd.ts
+++ rnd.ts
@@ -1,3 +1,14 @@
+import { Command } from "https://deno.land/x/cliffy@v0.24.2/command/mod.ts";
 import { randomNumber } from "./mod.ts";
 
-console.log(randomNumber());
+const command = new Command()
+  .name("rnd")
+  .version("1.0.0")
+  .description("Get a random number on your CLI.")
+  .option("-integer", "Integer?")
+  .option("-min <number:number>", "The minimum number.")
+  .option("-max <number:number>", "The Maximum number.");
+
+const { options } = await command.parse();
+
+console.log(randomNumber(options));
