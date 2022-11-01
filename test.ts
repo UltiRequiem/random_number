362d81d27218be6cfaf15abae61a83f209757c0c
GitHub <noreply@github.com>
2022-11-01 00:00:36 +0000

    ⬆️  Update dependencies

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@ import {
   assertEquals,
   assertNotEquals,
   assertThrows,
-} from "https://deno.land/std@0.158.0/testing/asserts.ts";
+} from "https://deno.land/std@0.161.0/testing/asserts.ts";
 import { randomNumber } from "./mod.ts";
 
 const between = (x: number, min: number, max: number) => x >= min && x <= max;
